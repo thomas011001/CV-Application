184cf54be62813ccc38e09e10c1e6da4fe1e017e
thomas <thomasyacoub321@gmail.com>
2025-05-16 08:49:27 +0300

    add first version

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import MyDocument from "./components/Cv";
+import Forms from "./components/Form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [personalInfo, setPersonalInfo] = useState({
+    fullName: "Thomas Yacoub",
+    phoneNumber: "010945",
+    email: "thomas@gmail.com",
+    gitHubUrl: "https://github.com",
+  });
+
+  const [education, setEducation] = useState([
+    {
+      university: "auc",
+      field: "computer scince degree",
+      degree: "Certificate",
+      date: "May 2023",
+    },
+  ]);
+
+  const [experience, setExperience] = useState([
+    {
+      experience: "Frontend Developer Internship",
+      description: "Built responsive UI using React and CSS",
+    },
+  ]);
+
+  const [skills, setSkills] = useState([
+    {
+      category: "Languages & Dev Tools",
+      skills: "js html css",
+    },
+  ]);
+
+  const [project, setProject] = useState([
+    {
+      project: "My Portfolio",
+      description: "HTML, CSS, responsive design, and animations",
+    },
+  ]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <span>By: Thomas Yacoub</span>
+        <span>
+          <a href="#">Github</a>
+        </span>
+      </nav>
+      <main>
+        <Forms
+          personalFormHandler={setPersonalInfo}
+          educationFormHandler={setEducation}
+          experienceFormHandler={setExperience}
+          skillsFormHandler={setSkills}
+          projectFormHandler={setProject}
+        />
+        <div className="panel">
+          <MyDocument
+            project={project}
+            skills={skills}
+            personalInfo={personalInfo}
+            education={education}
+            experience={experience}
+          />
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
